b4ba2bc9b1c577485a49cb0bedc19e03f798abb3
Tuckle <adi.piriu5113@yahoo.com>
2017-04-22 15:51:10 +0300

    Added navbar and played with maps api a little

--- URWeb/gba/gba.py
+++ URWeb/gba/gba.py
@@ -6,13 +6,18 @@ from django.views import generic, View
 from django.conf import settings
 import requests
 
-class Main(generic.TemplateView):
-	template_name = "gba.html"
 
-	def get(self, request, name):
-		url = requests.get(r'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&type=restaurant&keyword=cruise&key=AIzaSyDXYDYmpNXAo01aw71oMT6KJXoI1aTTyvg')
-		response = url.json()
-		return HttpResponse(str(response))
+class Main(generic.TemplateView):
+    template_name = "gba.html"
+
+    def get(self, request, name):
+        url = requests.get(
+            r'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-'
+            r'33.8670522,151.1957362&radius=500&type=restaurant&keyword=cruise&key=AIz'
+            r'aSyDXYDYmpNXAo01aw71oMT6KJXoI1aTTyvg')
+        response = url.json()
+        return HttpResponse(str(response))
+
 
 class Location(generic.TemplateView):
-	template_name = "location.html"
+    template_name = "location.html"
