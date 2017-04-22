0cb938f5baccb9ce57396f87025004827182e88d
Gheorghe BALAN <gbalan@ias.bitdefender.biz>
2017-04-22 19:36:33 +0300

    temp

--- URWeb/gba/gba.py
+++ URWeb/gba/gba.py
@@ -4,20 +4,119 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic, View
 from django.conf import settings
+import re
 import requests
-
+import time
+import json
+# import urllib
 
 class Main(generic.TemplateView):
-    template_name = "gba.html"
+	template_name = "gba.html"
 
-    def get(self, request, name):
-        url = requests.get(
-            r'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-'
-            r'33.8670522,151.1957362&radius=500&type=restaurant&keyword=cruise&key=AIz'
-            r'aSyDXYDYmpNXAo01aw71oMT6KJXoI1aTTyvg')
-        response = url.json()
-        return HttpResponse(str(response))
+	def constructData(self, googlePlacesResponse, apiKey, count = None):
+		data = []
+		resultCode = 0
+		firstResultCode = 0
+		counter = 0
+		while True:
+			if count and counter >= count:
+				break
 
+			if "status" in googlePlacesResponse:
+				if googlePlacesResponse['status'] == "OK":
+					resultCode = -1
+				elif googlePlacesResponse['status'] == "ZERO_RESULTS":
+					resultCode = 0
+				elif googlePlacesResponse['status'] == "OVER_QUERY_LIMIT":
+					resultCode = 1
+				elif googlePlacesResponse['status'] == "REQUEST_DENIED":
+					resultCode = 2
+				elif googlePlacesResponse['status'] == "INVALID_REQUEST":
+					resultCode = 3
+				else:
+					resultCode = 4
+
+				if counter == 0:
+					firstResultCode = resultCode
+				
+				if resultCode == -1:
+					if "results" in googlePlacesResponse:
+						for result in googlePlacesResponse["results"]:
+							data.append(result)
+
+					if "next_page_token" in googlePlacesResponse:
+						if len(googlePlacesResponse["next_page_token"]):
+							url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?pagetoken={}&key={}".format(googlePlacesResponse["next_page_token"], apiKey)
+							currentTime = time.time()
+							limitTime = currentTime + 10
+							while limitTime < time.time():
+								time.sleep(1)
+								ret = requests.get(url)
+								googlePlacesResponse = ret.json()
+								if googlePlacesResponse['status'] == "OK":
+									break
+						else:
+							break
+					else:
+						break
+				else:
+					break
+			else:
+				break
+			counter += 1
+
+		return firstResultCode, resultCode, data
+
+
+			
+
+	def get(self, request, name):
+		print str(request.body)
+		# url = requests.get(r'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&type=restaurant&keyword=cruise&key=AIzaSyDXYDYmpNXAo01aw71oMT6KJXoI1aTTyvg')
+		apiKey = "AIzaSyDXYDYmpNXAo01aw71oMT6KJXoI1aTTyvg"
+		# url = requests.get(r'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={},{}&radius=500&key={}'.format(lat, lng, apiKey))
+		url = requests.get(r'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={},{}&radius=500&key={}'.format("-33.8670522", "151.1957362", apiKey))
+		response = url.json()
+		firstResultCode, resultCode, data = self.constructData(response, apiKey, 2)
+		
+		# for items in response['results']:
+		# 	for types in items['types']:
+		# 		if types not in allTypes:
+		# 			allTypes.append(types)
+		result = "<html>{}<br>{}<br><ol>".format(firstResultCode, resultCode)
+		allTypes = []
+		for items in data:
+			result += "<li>" + str(items) + "</li>"
+		result += "</ol></html>"
+		return HttpResponse(str(result))
+
+	def post(self, request, name):
+		# url = requests.get(r'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&type=restaurant&keyword=cruise&key=AIzaSyDXYDYmpNXAo01aw71oMT6KJXoI1aTTyvg')
+		apiKey = "AIzaSyDXYDYmpNXAo01aw71oMT6KJXoI1aTTyvg"
+		# url = requests.get(r'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={},{}&radius=500&key={}'.format(lat, lng, apiKey))
+		url = requests.get(r'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={},{}&radius=500&key={}'.format(request.POST["lat"], request.POST["lng"], apiKey))
+		response = url.json()
+		firstResultCode, resultCode, data = self.constructData(response, apiKey, 2)
+		
+		# for items in response['results']:
+		# 	for types in items['types']:
+		# 		if types not in allTypes:
+		# 			allTypes.append(types)
+		# result = "<html>{}<br>{}<br><ol>".format(firstResultCode, resultCode)
+		# allTypes = []
+		# for items in data:
+		# 	result += "<li>" + str(items['name'].encode('ascii', 'ignore')) + "</li>"
+		# result += "</ol></html>"
+		# result = ""
+		# for items in data:
+		# 	result += items['name'].encode('ascii', 'ignore') + "\n"
+		allTypes = []
+		for item in data:
+			for types in item['types']:
+				if types not in allTypes:
+					allTypes.append(types)
+		result = allTypes
+		return HttpResponse(str(result))
 
 class Location(generic.TemplateView):
-    template_name = "location.html"
+	template_name = "location.html"
--- URWeb/gba/urls.py
+++ URWeb/gba/urls.py
@@ -21,11 +21,11 @@ from django.views.generic import TemplateView
 from .gba import Main, Location
 
 urls = [
-    url(r'^/temp/(?P<name>\w+)', Main.as_view(), name='temp'),
-    url(r'^/location', Location.as_view(), name='location')
-    #    url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
-    #    url(r'^admin',  admin.site.urls),
-    #    url(r'^login',  LoginView.as_view(template_name="login.html"), name='login'),
-    #    url(r'^signup', TemplateView.as_view(template_name='signup.html'), name='signup'),
-    #    url(r'^myaccount', TemplateView.as_view(template_name='myaccount.html'), name='myaccount')
+	url(r'^/temp(?:/(?P<name>\w+))?$', Main.as_view(), name = 'temp'),
+	url(r'^/location', Location.as_view(), name = 'location')
+#    url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
+#    url(r'^admin',  admin.site.urls),
+#    url(r'^login',  LoginView.as_view(template_name="login.html"), name='login'),
+#    url(r'^signup', TemplateView.as_view(template_name='signup.html'), name='signup'),		
+#    url(r'^myaccount', TemplateView.as_view(template_name='myaccount.html'), name='myaccount')
 ]
