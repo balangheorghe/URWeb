86b8c9aef97c1147b59be1c2b04d5fac84882d05
Gheorghe BALAN <gbalan@ias.bitdefender.biz>
2017-04-23 03:47:00 +0300

    saves

--- URWeb/api/apis/plugins.py
+++ URWeb/api/apis/plugins.py
@@ -3,11 +3,29 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
+import json
 
 
 class Plugins(generic.View):
-    def get(self, request, name, format):
-        return HttpResponse("OKOKOKOK")
+	def get(self, request, name, format):
+		result = ''
+		result += '<option>Find Nearby Person</option>'
+		result += '<option>Not find</option>'
+		result += '<option>Not null find</option>'
+		result += '<option>Find Nearby Person</option>'
+		result += '<option>Not find</option>'
+		result += '<option>Not null find</option>'
+		result += '<option>Find Nearby Person</option>'
+		result += '<option>Not find</option>'
+		result += '<option>Not null find</option>'
+		result += '<option>Find Nearby Person</option>'
+		result += '<option>Not find</option>'
+		result += '<option>Not null find</option>'
+		return HttpResponse(result)
 
-    def put(self, request):
-    	return HttpResponse("OK")
+	def put(self, request, name, format):
+		data = json.loads(request.body)
+		if data['name'] == 'adi_plugin':
+			return HttpResponse("OK")
+		else:
+			return HttpResponse("Bad plugin name")
